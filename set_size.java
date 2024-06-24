e1cf6bb62cd37cbcbaf1a96e659aeed88d92cbb2
DhakshnamoorthyPalanisamy <166498759+DhakshnamoorthyPalanisamy@users.noreply.github.com>
2024-06-24 15:31:00 +0530

    Add files via upload

---
+++ set_size.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+public class set_size {
+    public static void main(String args[]){
+        LinkedList<String> color=new LinkedList<>();
+        color.add("blue");
+        color.add("yellow");
+        color.add("red");
+        color.add("pink");
+        color.add("green");
+        System.out.println("The Size of the Element: " + color.size());
+        color.set(2,"grey");
+        System.out.println("Replace the Element:" + color);
+    }
+    
+}
